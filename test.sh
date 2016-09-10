4f01c9e9977c6c2298ea7d1a94bdf978150adc75
Martin Fracker <martin.frackerjr@gmail.com>
2016-09-09 20:59:40 -0500

    Make sure out file is gone after testing

--- test.sh
+++ test.sh
@@ -3,9 +3,10 @@
 SAMPLEIN=$(find . -name '*.frag' -o \( -name '*.decaf' \))
 
 for SAMPLE in $SAMPLEIN; do
-    rm -f out
     EXPECTEDOUT=$(echo "$SAMPLE" | sed -e "s/\.frag/\.out/g" | sed -e "s/\.decaf/\.out/g" )
 
+    rm -f out
     ./dcc < $SAMPLE > out 2>&1
     cmp --silent out $EXPECTEDOUT || echo "Test failed for output $EXPECTEDOUT"
+    rm out
 done
